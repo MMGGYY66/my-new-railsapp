719ee89bd29057c3c65ca49b89e634673844de2e
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-04-09 14:22:54 -0500

    update

---
+++ the_debugger_and_binding_break_methods.rb
@@ -0,0 +1,13 @@
+require "debug"
+
+candy = "Sour Patch Kids"
+puts "I love eating #{candy}"
+
+debugger
+
+beverage = "Arizona Iced Tea"
+puts "I love drinking #{beverage}"
+
+debugger
+
+puts "This is the end of the program"
